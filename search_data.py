3c40388425d6add5879cb91a45d5bafe791e3eb9
Nick Soto Conde <nicksc1911@gmail.com>
2022-02-04 11:24:38 -0500

    Update

--- search_data.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class Option(Enum):
-    category = {'name':'Category','object':'product.category', 'field':['name', 'product_count']} 
-    brand = {'name':'Brand','object':'product.brand', 'field':['internal_code','name', 'product_count']}
-    supplier = {'name':'Supplier','object':'res.partner', 'field':["name", "purchase_order_count"]}
