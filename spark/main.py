77b5e04db9fd71638a7cb9276b301a8357bc80a3
Nick Soto Conde <nicksc1911@gmail.com>
2022-02-08 16:05:21 -0500

    Spark - Print Data

--- spark/main.py
+++ spark/main.py
@@ -9,21 +9,22 @@ Lectura de archivos .CSV
 """
 # df_brand = spark.read.csv("csv_data/Brand.csv")
 df_brand = spark.read.option('header', 'true').csv("csv_data/Brand.csv")
-df_brand.show()
-print("Cantidad de Marcas registradas: " + str(df_brand.count()))
+df_brand.show(30)
+print(type(df_brand))
+print("Cantidad de Marcas registradas: " + str(df_brand.count()) + "\n")
 
 df_category = spark.read.option('header', 'true').csv("csv_data/Category.csv")
-df_category.show()
-print("Cantidad de Categorias registradas: " + str(df_category.count()))
+df_category.show(30)
+print("Cantidad de Categorias registradas: " + str(df_category.count())+"\n")
 
 df_supplier = spark.read.option('header', 'true').csv("csv_data/Supplier.csv")
-df_supplier.show()
-print("Cantidad de Proveedores registrados: " + str(df_supplier.count()))
+df_supplier.show(30)
+print("Cantidad de Proveedores registrados: " + str(df_supplier.count())+"\n")
 
 """
-Escritura dearchivos .PARQUET
+Escritura de archivos .PARQUET
 """
 
-df_brand.write.parquet("parquet_data/Brand.parquet")
-df_category.write.parquet("parquet_data/Category.parquet")
-df_supplier.write.parquet("parquet_data/Supplier.parquet")
+#df_brand.write.parquet("parquet_data/Brand.parquet")
+#df_category.write.parquet("parquet_data/Category.parquet")
+#df_supplier.write.parquet("parquet_data/Supplier.parquet")
